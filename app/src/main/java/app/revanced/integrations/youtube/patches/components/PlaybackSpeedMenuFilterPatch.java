dabbc3de3a1e2134314b8c6bcc84ebdaed896f75
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-10-25 06:35:06 -0400

    fix(YouTube - Playback speed): Restore old playback speed menu

--- app/src/main/java/app/revanced/integrations/youtube/patches/components/PlaybackSpeedMenuFilterPatch.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/components/PlaybackSpeedMenuFilterPatch.java
@@ -8,20 +8,41 @@ import app.revanced.integrations.youtube.patches.playback.speed.CustomPlaybackSp
  * Abuse LithoFilter for {@link CustomPlaybackSpeedPatch}.
  */
 public final class PlaybackSpeedMenuFilterPatch extends Filter {
-    // Must be volatile or synchronized, as litho filtering runs off main thread and this field is then access from the main thread.
-    public static volatile boolean isPlaybackSpeedMenuVisible;
+
+    /**
+     * Old litho based speed selection menu.
+     */
+    public static volatile boolean isOldPlaybackSpeedMenuVisible;
+    /**
+     * 0.05x speed selection menu.
+     */
+    public static volatile boolean isPlaybackRateSelectorMenuVisible;
+
+    private final StringFilterGroup playbackRateSelector;
 
     public PlaybackSpeedMenuFilterPatch() {
-        addPathCallbacks(new StringFilterGroup(
+        // 0.05x speed menu.
+        playbackRateSelector = new StringFilterGroup(
                 null,
-                "playback_speed_sheet_content.eml-js"
-        ));
+                "playback_rate_selector_menu_sheet.eml-js"
+        );
+
+        // Old litho based speed menu.
+        var oldPlaybackMenu = new StringFilterGroup(
+                null,
+                "playback_speed_sheet_content.eml-js");
+
+        addPathCallbacks(playbackRateSelector, oldPlaybackMenu);
     }
 
     @Override
     boolean isFiltered(@Nullable String identifier, String path, byte[] protobufBufferArray,
                        StringFilterGroup matchedGroup, FilterContentType contentType, int contentIndex) {
-        isPlaybackSpeedMenuVisible = true;
+        if (matchedGroup == playbackRateSelector) {
+            isPlaybackRateSelectorMenuVisible = true;
+        } else {
+            isOldPlaybackSpeedMenuVisible = true;
+        }
 
         return false;
     }
